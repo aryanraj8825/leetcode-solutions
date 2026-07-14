54880553ef60ba943553ffa06dad932a6fad812a
Aryan raj <aryanrshahi62@gmail.com>
2026-07-14 16:12:03 +0530

    Time: 573 ms (13.89%) | Memory: 58.4 MB (50.00%) - LeetSync

---
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int subsequencePairCount(int[] nums) {
+        int MOD = 1_000_000_007;
+        int MAX = 200;
+
+        long[][] dp = new long[MAX + 1][MAX + 1];
+        dp[0][0] = 1;
+
+        for (int x : nums) {
+            long[][] next = new long[MAX + 1][MAX + 1];
+
+            for (int g1 = 0; g1 <= MAX; g1++) {
+                for (int g2 = 0; g2 <= MAX; g2++) {
+                    if (dp[g1][g2] == 0) continue;
+
+                    long ways = dp[g1][g2];
+
+                    next[g1][g2] = (next[g1][g2] + ways) % MOD;
+
+                    int ng1 = gcd(g1, x);
+                    next[ng1][g2] = (next[ng1][g2] + ways) % MOD;
+
+                    int ng2 = gcd(g2, x);
+                    next[g1][ng2] = (next[g1][ng2] + ways) % MOD;
+                }
+            }
+
+            dp = next;
+        }
+
+        long ans = 0;
+
+        for (int g = 1; g <= MAX; g++) {
+            ans = (ans + dp[g][g]) % MOD;
+        }
+
+        return (int) ans;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
